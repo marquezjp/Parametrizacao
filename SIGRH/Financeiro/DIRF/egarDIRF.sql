27bfd06be64ac598b0e993aef974144df09c9882
João Paulo Marquez <jpvillela@indracompany.com>
2021-03-20 10:27:51 -0300

    DIRF

--- SIGRH/Financeiro/DIRF/egarDIRF.sql
+++ SIGRH/Financeiro/DIRF/egarDIRF.sql
@@ -1,11 +1,36 @@
 select *
-from egardadoanaliticodirf;
-
+from egardadoanaliticodirf
+where nucpfbeneficiario = 02442291479
 select *
-from egararquivoconsoldirf;
-
+from egararquivoconsoldirf
+where nucpfbeneficiario = 02442291479
 select *
-from egarcomprovanterendimento;
-
-select *
-from egarhistoricoobrigacao;
+from egarcomprovanterendimento
+where cdvinculo in (
+        select v.cdvinculo
+        from ecadvinculo v
+            inner join ecadpessoa p on p.cdpessoa = v.cdpessoa
+        where p.nucpf = 02442291479
+    );
+select tpo.nmtipoobrigacaolegal,
+    tpo.flvisivel,
+    ol.nuanocompetencia,
+    ol.nuanoreferencia,
+    ol.dtprazoentrega,
+    ol.delayout,
+    ol.dtultalteracao,
+    o.sgorgao,
+    oh.nuprotocoloentrega,
+    oh.dtsolicitacao,
+    oh.dtgeracao,
+    oh.dtbaixa,
+    oh.dtentrega,
+    oh.nucpfcadastrador,
+    oh.dtinclusao,
+    oh.demotivointerrupcao,
+    oh.flconferencialiberada,
+    oh.flportalliberado
+from egarhistoricoobrigacao oh
+    inner join egarobrigacaolegal ol on ol.cdobrigacaolegal = oh.cdobrigacaolegal
+    inner join egartipoobrigacaolegal tpo on tpo.cdtipoobrigacaolegal = ol.cdtipoobrigacaolegal
+    inner join vcadorgao o on o.cdorgao = oh.cdorgao;
