ef0a4dc5433bdbb00cfb8cc1c2c73bbe7dcd6d73
João Paulo Marquez <jpvillela@indracompany.com>
2021-04-27 17:37:57 -0300

    Migracao Função de Chefia

--- SIGRH/Migracao/CarreiraValor/FuncaoChefia.sql
+++ SIGRH/Migracao/CarreiraValor/FuncaoChefia.sql
@@ -3,33 +3,89 @@ select
  poder.sgpoder as sigla_do_poder,
  agrup.sgagrupamento as sigla_agrupamento_de_orgao,
  orgao.sgorgao as sigla_do_orgao,
-
- --- Funcao de Chefia ---
- evolucao.nmfuncaochefia,
- evolucao.deevolucao,
- tipo.nmtipofuncaochefia,
- padrao.nmpadrao,
- padrao.depadrao,
-
- --- Propriedade ---
- case when evolucao.cdorgao is null then 'DO AGRUPAMENTO' else 'DO ORGAO' end as gestao,
- ref.qtunidade,
- ref.vlfuncao,
- ref.cdevolucaofucvalorref
  
-from ecadevolucaofuncaochefia evolucao
-left join ecadevolucaofucvalorref ref on ref.cdevolucaofuncaochefia = evolucao.cdevolucaofuncaochefia
-left join epagpadraofucagrup padrao on padrao.cdpadraofucagrup = ref.cdpadraofucagrup
+ --- Informações Principais ---
+ upper(evolucaoefc.nmfuncaochefia) as funcao_chefia,
+ case when evolucaoefc.cdorgao is null then 'DO AGRUPAMENTO' else 'DO ORGAO' end as tipo_gestao_funcao_chefia,
+ evolucaoefc.flfuncaogratificada as funcao_gratificada,
+ evolucaoefc.dtiniciovigencia as inicio_vigencia_funcao_chefia,
+ evolucaoefc.dtfimvigencia as fim_vigencia_funcao_chefia,
+ valorefc.vlfixo as valor_padrao_funcao_chefia,
+ padraoefc.nmpadrao as padrao_funcao_chefia,
+ padraoefc.depadrao as descricao_padrao_funcao_chefia,
+ tpefc.nmtipofuncaochefia as tipo_funcao_chefia,
+ upper(cntefc.nmconceitocarreira) as conceito_carreira_fc,
+ evolucaoefc.flquadrolotacional as utiliza_quadro_lotacional_fc,
+ upper(qlpefc.nmdescricaoqlp) as quadro_cargos_funcao_chefia,
+ upper(reltrabqlpefc.nmrelacaotrabalho) as rel_trab_quadro_cargos_fc,
 
-left join ecadtipofuncaochefia tipo on tipo.cdtipofuncaochefia = evolucao.cdtipofuncaochefia
+ --- Propriedades ---
+ upper(tpchefc.nmtipocargahoraria) as tipo_da_carga_horaria_fc,
+ choefc.nucargahoraria as carga_horaia_padrao_fc,
+ evolucaoefc.flaumentocarga as permite_aumento_cho_fc,
+ evolucaoefc.vlreducaocarga as perc_limite_reducao_cho_fc,
 
-left join vcadorgao orgao on orgao.cdorgao = evolucao.cdorgao
+ evolucaoefc.vlpercdedicacaoexclusiva as perc_dedicacao_excl_magist_fc,
+ evolucaoefc.flproporcionalizacho as proporcionalizar_por_cho_fc,
+ evolucaoefc.flregenciaclasse as direito_regencia_classe_fc,
 
-left join ecadagrupamento agrup on agrup.cdagrupamento = evolucao.cdagrupamento
+ evolucaoefc.flacumulada as permite_acumular_funcao_chefia,
+ evolucaoefc.flmovimentacaodefinitiva as permite_moviment_definitiva_fc,
+ evolucaoefc.flgerarlocaltrabalho as novo_local_trab_moviment_fc,
+ evolucaoefc.flpermitesubstituicao as permite_substituicao_fc,
+ evolucaoefc.flhierarqigualsuperior as subst_hierarquia_ig_ou_sup_fc,
+
+ evolucaoefc.flregprofissional as necessidade_reg_prof_fc,
+ evolucaoefc.flhabilitacao as necessidade_habilitacao_fc,
+ evolucaoefc.flocupadaestagioprob as pert_ocupada_em_estag_prob_fc,
+ evolucaoefc.flmilitar as funcao_chefia_militar_fc,
+ evolucaoefc.flestritamentepolicial as estritamente_policial_fc,
+ tptmpefc.nmtempoefeitocontagem as tempo_efeito_contagem_fc,
+
+ --evolucaoefc.cdfucabsorvervagas,
+ 
+ tpuoefc.nmtipounidorg as tipo_unidade_organizacional_fc,
+ tpuoefc.flensino as tipo_instituicao_ensino_fc,
+ tpuoefc.flescola as tipo_escola_fc,
+ tpuoefc.flfilial as tipo_filial_fc,
+ 
+ refefc.qtunidade as quantidade_unidade_valor_fc,
+ refefc.vlfuncao as valor_funcao_chefia,
+
+ upper(evolucaoefc.deevolucao) as descricao_evolucao_fc,
+
+ --- Valor do Padrao --
+ versaoefc.nuversao as versao_tabela_valor_fc,
+ lpad(histefc.nuanoiniciovigencia, 4, 0) || lpad(histefc.numesiniciovigencia, 2, 0) as anomes_inicio_vig_valor_fc,
+ lpad(histefc.nuanofimvigencia, 4, 0) || lpad(histefc.numesfimvigencia, 2, 0) as anomes_fim_vig_valor_fc,
+ valorefc.deexpressaocalculo as descricao_expressao_calculo_fc
+
+from ecadevolucaofuncaochefia evolucaoefc
+left join ecadevolucaofucvalorref refefc on refefc.cdevolucaofuncaochefia = evolucaoefc.cdevolucaofuncaochefia
+left join epagpadraofucagrup padraoefc on padraoefc.cdpadraofucagrup = evolucaoefc.cdpadraofucagrup
+left join ecadevolucaofucitemcargahor choefc on choefc.cdevolucaofuncaochefia = evolucaoefc.cdevolucaofuncaochefia and choefc.flpadrao = 'S'
+
+--- Valor Padrao Funcao Chefia ---
+left join epagvalorreffucagruporgespec valorefc on valorefc.cdpadraofucagrup = padraoefc.cdpadraofucagrup
+left join epaghistvalorreffucagruporg histefc on histefc.cdhistvalorreffucagruporg = valorefc.cdhistvalorreffucagruporg
+left join epagvalorreffucagruporgversao versaoefc on versaoefc.cdvalorreffucagruporgversao = histefc.cdvalorreffucagruporgversao
+
+--- Dominio Estrutura de Carreira Funcao Chefia ---
+left join ecadtipofuncaochefia tpefc on tpefc.cdtipofuncaochefia = evolucaoefc.cdtipofuncaochefia
+left join ecadconceitocarreira cntefc on cntefc.cdconceitocarreira = evolucaoefc.cdconceitocarreira
+left join emovdescricaoqlp qlpefc on qlpefc.cddescricaoqlp = evolucaoefc.cddescricaoqlp
+left join ecadrelacaotrabalho reltrabqlpefc on reltrabqlpefc.cdrelacaotrabalho = qlpefc.cdrelacaotrabalho
+left join ecadtipocargahoraria tpchefc on tpchefc.cdtipocargahoraria = evolucaoefc.cdtipocargahoraria
+left join ecadtipounidorg tpuoefc on tpuoefc.cdtipounidorg = evolucaoefc.cdtipounidorg
+left join epvdtempoefeitocontagem tptmpefc on tptmpefc.cdtempoefeitocontagem = evolucaoefc.cdtempoefeitocontagem
+
+--- Agrupamento ---
+left join vcadorgao orgao on orgao.cdorgao = evolucaoefc.cdorgao
+left join ecadagrupamento agrup on agrup.cdagrupamento = evolucaoefc.cdagrupamento
 left join ecadpoder poder on poder.cdpoder = agrup.cdpoder
 
 order by 
+ poder.sgpoder,
+ agrup.sgagrupamento,
  nvl(orgao.sgorgao, ' '),
- evolucao.nmfuncaochefia,
- evolucao.deevolucao
- 
+ evolucaoefc.nmfuncaochefia
--- SIGRH/Migracao/CarreiraValor/FuncaoChefiaValor.sql
+++ SIGRH/Migracao/CarreiraValor/FuncaoChefiaValor.sql
@@ -1,23 +1,41 @@
+define dataRef = sysdate
+define dtfim = TO_DATE('01/04/2021')
+define anoMesRef = extract(year from &dataRef) || lpad(extract(month from &dataRef), 2, 0)
+;
+select *
+from (
 select
- versao.nuversao,
- padrao.nmpadrao,
- padrao.depadrao,
- lpad(hist.nuanoiniciovigencia, 4, 0) || lpad(hist.numesiniciovigencia, 2, 0) as nuanomesiniciovigencia,
- lpad(hist.nuanofimvigencia, 4, 0) || lpad(hist.numesfimvigencia, 2, 0) as nuanomesfimvigencia,
- valor.vlfixo,
- valor.deexpressaocalculo
+ --- Agrupamento ---
+ poder.sgpoder as sigla_do_poder,
+ agrup.sgagrupamento as sigla_agrupamento_de_orgao,
+ orgao.sgorgao as sigla_orgao,
  
-from epagpadraofucagrup padrao
-left join epagvalorreffucagruporgespec valor on valor.cdpadraofucagrup = padrao.cdpadraofucagrup
-left join epaghistvalorreffucagruporg hist on hist.cdhistvalorreffucagruporg = valor.cdhistvalorreffucagruporg
-left join epagvalorreffucagruporgversao versao on versao.cdvalorreffucagruporgversao = hist.cdvalorreffucagruporgversao
+ --- Valor do Padrao Funcao Chefia ---
+ padraoefc.nmpadrao as padrao_tab_valor_fc,
+ padraoefc.depadrao as descricao_padrao_tab_valor_fc,
+ versaoefc.nuversao as versao_tab_valor_fc,
+ lpad(histefc.nuanoiniciovigencia, 4, 0) || lpad(histefc.numesiniciovigencia, 2, 0) as anomes_inicio_vig_tab_valor_fc,
+ lpad(histefc.nuanofimvigencia, 4, 0) || lpad(histefc.numesfimvigencia, 2, 0) as anomes_fim_vig_tab_valor_fc,
+ valorefc.vlfixo as valor_padrao_tab_valor_fc,
+ valorefc.deexpressaocalculo as expressao_calculo_tab_valor_fc
 
---where lpad(hist.nuanofimvigencia, 4, 0) || lpad(hist.numesfimvigencia, 2, 0) is null
+--- Valor do Padrao Funcao Chefia --- 
+from epagpadraofucagrup padraoefc
+inner join epagvalorreffucagruporgespec valorefc on valorefc.cdpadraofucagrup = padraoefc.cdpadraofucagrup
+inner join epaghistvalorreffucagruporg histefc on histefc.cdhistvalorreffucagruporg = valorefc.cdhistvalorreffucagruporg
+inner join epagvalorreffucagruporgversao versaoefc on versaoefc.cdvalorreffucagruporgversao = histefc.cdvalorreffucagruporgversao
+left join vcadorgao orgao on orgao.cdorgao = versaoefc.cdorgao
+inner join ecadagrupamento agrup on agrup.cdagrupamento = versaoefc.cdagrupamento
+inner join ecadpoder poder on poder.cdpoder = agrup.cdpoder
 
 order by
- versao.nuversao,
- padrao.nmpadrao,
- hist.nuanoiniciovigencia,
- hist.numesiniciovigencia,
- hist.nuanofimvigencia,
- hist.numesfimvigencia
+ poder.sgpoder,
+ agrup.sgagrupamento,
+ orgao.sgorgao,
+ padraoefc.nmpadrao,
+ versaoefc.nuversao,
+ histefc.nuanoiniciovigencia,
+ histefc.numesiniciovigencia
+) tab_valor_fc
+where nvl(tab_valor_fc.anomes_inicio_vig_tab_valor_fc,&anoMesRef) <= nvl(extract(year from &dtfim) || lpad(extract(month from &dtfim), 2, 0),&anoMesRef)
+  and nvl(tab_valor_fc.anomes_fim_vig_tab_valor_fc,&anoMesRef) >= nvl(extract(year from &dtfim) || lpad(extract(month from &dtfim), 2, 0),&anoMesRef)
