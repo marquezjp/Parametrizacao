33ad2a05681fb49b1c08f0fd903b29fe9fe4e5fe
João Paulo Marquez <jpvillela@indracompany.com>
2022-05-24 15:34:44 -0300

    Excluir Usuário Atuak]lizado

--- SIGRH/Segurança/cdUsuario.sql
+++ SIGRH/Segurança/cdUsuario.sql
@@ -21,30 +21,38 @@ where cdusuariologado in (2938, 17609, 5775, 28172);
 
 -- Excluir --
 
-delete from eseglogcampo
-where cdlogtabela in (select cdlogtabela from eseglogtabela
-                       where cdlog in (select cdlog from eseglog
-                                        where cdautorizacaoacesso in (select cdautorizacaoacesso from esegautorizacaoacesso
-                                                                       where cdusuario = 1000002)));
+define usuario = (select cdusuario from esegusuario where nucpf = '51902290259');
+
+select * from esegusuario
+where cdusuario = &usuario;
 
 delete from eseglogtabela
 where cdlog in (select cdlog from eseglog
-                 where cdautorizacaoacesso in (select cdautorizacaoacesso from esegautorizacaoacesso where cdusuario = 1000002));
+                 where cdautorizacaoacesso in (select cdautorizacaoacesso from esegautorizacaoacesso where cdusuario = &usuario));
 
 delete from eseglog
-where cdautorizacaoacesso in (select cdautorizacaoacesso from esegautorizacaoacesso where cdusuario = 1000002);
+where cdautorizacaoacesso in (select cdautorizacaoacesso from esegautorizacaoacesso where cdusuario = &usuario);
 
 delete from eseghistsenhaacesso
-where cdautorizacaoacesso in (select cdautorizacaoacesso from esegautorizacaoacesso where cdusuario = 1000002);
+where cdautorizacaoacesso in (select cdautorizacaoacesso from esegautorizacaoacesso where cdusuario = &usuario);
 
 delete from esegfuncionalidadegestor
-where cdautorizacaoacesso in (select cdautorizacaoacesso from esegautorizacaoacesso where cdusuario = 1000002);
+where cdautorizacaoacesso in (select cdautorizacaoacesso from esegautorizacaoacesso where cdusuario = &usuario);
 
 delete from esegautorizacaoabrangencia
-where cdautorizacaoacesso in (select cdautorizacaoacesso from esegautorizacaoacesso where cdusuario = 1000002);
+where cdautorizacaoacesso in (select cdautorizacaoacesso from esegautorizacaoacesso where cdusuario = &usuario);
 
 delete from esegautorizacaoperfil
-where cdautorizacaoacesso in (select cdautorizacaoacesso from esegautorizacaoacesso where cdusuario = 1000002);
+where cdautorizacaoacesso in (select cdautorizacaoacesso from esegautorizacaoacesso where cdusuario = &usuario);
 
 delete from esegautorizacaoacesso
-where cdusuario = 1000002;
+where cdusuario = &usuario;
+
+delete from eseghistsenhaacesso
+where cdusuario = &usuario;
+
+delete from esegusuariohistorico
+where cdusuario = &usuario;
+
+delete from esegusuario
+where cdusuario = &usuario;
