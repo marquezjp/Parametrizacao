e55d6bf0758113a43e31c7c6c3f6fcb349bcb46c
João Paulo Marquez <jpvillela@indracompany.com>
2022-10-08 11:42:12 -0300

    Listar todas as Sequences

--- PL-SQL/Sequences.sql
+++ PL-SQL/Sequences.sql
@@ -19,3 +19,12 @@ begin
   exit when i >= last_number;
   end loop;
 end;
+/
+
+select tab.table_name  as Tab, seq.sequence_name  as Seq, col.column_name as Col, seq.last_number as Last
+from user_tables tab
+inner join user_sequences seq on seq.sequence_name = 'S' || Substr(tab.table_name, 2, 250)
+inner join user_tab_columns col on col.table_name = tab.table_name and col.column_id = 1
+where substr(tab.table_name,1,1) = 'E'
+order by tab.table_name
+;
