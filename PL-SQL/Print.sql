026cfd0e8b209d4c219229d0e68d86963060ad36
João Paulo Marquez <jpvillela@indracompany.com>
2022-10-08 14:08:55 -0300

    Print

--- PL-SQL/Print.sql
+++ PL-SQL/Print.sql
@@ -3,9 +3,7 @@ set serveroutput on
 declare
 
 procedure print (p in varchar2) is
-begin
-  dbms_output.put_line(p);
-end;
+begin dbms_output.put_line(p); end;
 
 function f1 return varchar2 is
 begin
