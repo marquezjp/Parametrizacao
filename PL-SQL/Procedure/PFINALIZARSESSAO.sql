8b72d54b47976f60f01b2b29d53170fc978e872e
João Paulo Marquez <jpvillela@indracompany.com>
2022-10-22 12:45:57 -0400

    Exemplo de Pesquisar Sessão

--- PL-SQL/Procedure/PFINALIZARSESSAO.sql
+++ PL-SQL/Procedure/PFINALIZARSESSAO.sql
@@ -1,6 +1,21 @@
+-- Atalho no Teclado para Nova Sessão Não Compartilhada (unshared connection) CTRL+SHIFT+N
+
+-- Configuração do SQL Developer para Nova Sessão Não Compartilhada (unshared connection)
+-- - Tools       ==> Preferences  ==> Database       ==> Worksheet ==> New Worksheet to use unshared connect
+-- - Ferramentas ==> Preferencias ==> Banco de Dados ==> Planilha  ==> Nova Planilha para usar conexão não compartilhada
+
+-- Verificar as Sessões de um Usuário
 select sid, serial# from V$SESSION
 where osuser = 'jpvillela';
 /
 
+-- Verificar o Numero da Sessão Abertar no SQL Developer
+select s.sid, s.serial#
+from V$SESSION s, V$PROCESS p
+where s.audsid = sys_context('userenv','sessionid')
+  and s.paddr = p.addr;
+/
+
+-- Finalizar uma Sessão
 begin PFINALIZARSESSAO(879, 45553); end;
 /
