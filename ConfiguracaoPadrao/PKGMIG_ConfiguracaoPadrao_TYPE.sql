a45ecacc3865058b4205b8d314db38a13bd53c56
João Paulo Marquez <joaopaulo.marquez@gmail.com>
2025-06-15 12:26:07 -0300

    Separação dos Arquivos dos Pacotes em Declaração e Corpo, além dos Tipos de Dados.

---
+++ ConfiguracaoPadrao/PKGMIG_ConfiguracaoPadrao_TYPE.sql
@@ -0,0 +1,83 @@
+--- Pacote de Exportação e Importação das Configurações Padrão
+
+DROP TYPE tpConfiguracaoResumoTabela;
+DROP TYPE tpConfiguracaoListarTabela;
+DROP TYPE tpConfiguracaoLogResumoTabela;
+DROP TYPE tpConfiguracaoLogResumoEntidadesTabela;
+DROP TYPE tpConfiguracaoLogListarTabela;
+/
+
+CREATE OR REPLACE TYPE tpConfiguracaoResumo AS OBJECT (
+-- Tipo Objeto: Resumo da Configuração Padrão
+  sgAgrupamento   VARCHAR2(15),
+  sgOrgao         VARCHAR2(15),
+  sgModulo        VARCHAR2(3),
+  sgConceito      VARCHAR2(20),
+  dtExportacao    TIMESTAMP,
+  nuConteudos     NUMBER
+);
+
+CREATE OR REPLACE TYPE tpConfiguracaoListar AS OBJECT (
+-- Tipo Objeto: Listar Configuração Padrão
+  sgAgrupamento   VARCHAR2(15),
+  sgOrgao         VARCHAR2(15),
+  sgModulo        VARCHAR2(3),
+  sgConceito      VARCHAR2(20),
+  dtExportacao    TIMESTAMP,
+  cdIdentificacao VARCHAR2(20), 
+  jsConteudo      CLOB
+);
+
+
+CREATE OR REPLACE TYPE tpConfiguracaoLogResumo AS OBJECT (
+-- Tipo Objeto: Resumo do Log das Operações de Exportação e Importação das Configuração
+  tpOperacao      VARCHAR2(15),
+  dtOperacao      VARCHAR2(25),
+  sgAgrupamento   VARCHAR2(15),
+  sgOrgao         VARCHAR2(15),
+  sgModulo        VARCHAR2(3),
+  sgConceito      VARCHAR2(20)
+);
+ 
+CREATE OR REPLACE TYPE tpConfiguracaoLogResumoEntidades AS OBJECT (
+-- Tipo Objeto: Resumo do Log das Operações de Exportação e Importação das Configuração por Entidades
+  tpOperacao      VARCHAR2(15),
+  dtOperacao      VARCHAR2(25),
+  sgAgrupamento   VARCHAR2(15),
+  sgOrgao         VARCHAR2(15),
+  sgModulo        VARCHAR2(3),
+  sgConceito      VARCHAR2(20),
+  nmEntidade      VARCHAR2(50),
+  nuEventos       NUMBER,
+  nuRegistros     NUMBER
+);
+/
+
+CREATE OR REPLACE TYPE tpConfiguracaoLogListar AS OBJECT (
+-- Tipo Objeto: Listar o Log da Operação de Exportação ou Importação da Configuração
+  tpOperacao      VARCHAR2(15),
+  dtOperacao      VARCHAR2(25),
+  sgAgrupamento   VARCHAR2(15),
+  sgOrgao         VARCHAR2(15),
+  sgModulo        VARCHAR2(3),
+  sgConceito      VARCHAR2(20),
+  nmEntidade      VARCHAR2(50),
+  cdIdentificacao VARCHAR2(50),
+  nmEvento        VARCHAR2(50),
+  nuRegistros     NUMBER,
+  deMensagem      VARCHAR2(4000),
+  dtInclusao      TIMESTAMP(6)
+ );
+/
+
+-- Tipo Tabela: Resumo da Configuração Padrão
+CREATE OR REPLACE TYPE tpConfiguracaoResumoTabela AS TABLE OF tpConfiguracaoResumo;
+-- Tipo Tabela: Listar Configuração Padrão 
+CREATE OR REPLACE TYPE tpConfiguracaoListarTabela AS TABLE OF tpConfiguracaoListar;
+-- Tipo Tabela: Resumo do Log das Operações de Exportação e Importação das Configuração
+CREATE OR REPLACE TYPE tpConfiguracaoLogResumoTabela AS TABLE OF tpConfiguracaoLogResumo;
+-- Tipo Tabela: Resumo do Log das Operações de Exportação e Importação das Configuração por Entidades
+CREATE OR REPLACE TYPE tpConfiguracaoLogResumoEntidadesTabela AS TABLE OF tpConfiguracaoLogResumoEntidades;
+-- Tipo Tabela: Listar o Log da Operação de Exportação ou Importação da Configuração
+CREATE OR REPLACE TYPE tpConfiguracaoLogListarTabela AS TABLE OF tpConfiguracaoLogListar;
+/
